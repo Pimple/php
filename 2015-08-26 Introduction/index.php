07aa0c8fde18dbbb09ca79991ca852d199fb2eae
Pimple <ferociouscookiemonster@gmail.com>
2015-08-27 21:54:11 +0200

    Introduction assignment now works as intended

--- 2015-08-26 Introduction/index.php
+++ 2015-08-26 Introduction/index.php
@@ -23,7 +23,7 @@
 				'L' => 50, 'XL' => 40, 'X' => 10, 'V' => 5, 'IV' => 4, 'I' => 1);
 
 		$n = str_split($romanNumber, 1);
-		for ($i = count($n); $i == 0; $i--)
+		for ($i = count($n) - 1; $i >= 0; $i--)
 		{
 			if ($i > 0 && isset($lookup[$n[$i - 1] . $n[$i]]))
 			{
@@ -33,7 +33,6 @@
 			else
 				$result += $lookup[$n[$i]];
 		}
-
 		return $result;
 	}
 
@@ -47,8 +46,23 @@
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
 	<title>Normal to Roman integer converter</title>
+	<style type="text/css">
+		body
+		{
+			margin: 100px;
+			font-family: Consolas;
+			font-size: 16px;
+		}
+		a
+		{
+			color: black;
+			font-weight: bold;
+			text-decoration: none;
+		}
+	</style>
 </head>
 <body>
+	<h1><a href="https://youtu.be/3yDF67SQyFs?t=40s" target="_blank">Unremarkable header</a></h1><br>
 	<form name="myForm" method="post" action="index.php">
 		<label for="year">Type a year</label>
 		<input type="text" name="year" id="year"/>
@@ -59,8 +73,14 @@
 	</form>
 	<?php
 		if (isset($year))
+		{
 			echo "<p>$year = " . romanize($year) . "</p>";
+			echo '<script type="text/javascript">' . PHP_EOL;
+			echo '  document.myForm.year.focus()' . PHP_EOL;
+			echo '</script>' . PHP_EOL;
+		}
 	?>
+	<br>
 	<form name="myOtherForm" method="post" action="index.php">
 		<label for="romanYear">Type a year in Roman</label>
 		<input type="text" name="romanYear" id="romanYear"/>
@@ -71,7 +91,12 @@
 	</form>
 	<?php
 		if (isset($romanYear))
-			echo "$romanYear = " . numeralize($romanYear) . "</p>";
+		{
+			echo "<p>$romanYear = " . numeralize($romanYear) . "</p>";
+			echo '<script type="text/javascript">' . PHP_EOL;
+			echo '  document.myOtherForm.romanYear.focus()' . PHP_EOL;
+			echo '</script>' . PHP_EOL;
+		}
 	?>
 
 	<script type="text/javascript">
