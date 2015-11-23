6790a81b6ebf2f43d40920351c262ff8686cf9fe
Pimple <ferociouscookiemonster@gmail.com>
2015-11-23 22:38:53 +0100

    PHP Exam Project

---
+++ Exam/application/views/main_view.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>The weather forecast is FABULOUS!</title>
+	<link rel="stylesheet" href="stylish/bootstrap.min.css">
+	<link rel="stylesheet" href="stylish/bootstrap-responsive.css">
+	<link rel="stylesheet" href="stylish/bootswatch.css">
+	<link rel="stylesheet" href="stylish/prettify.css">
+	<link rel="stylesheet" href="stylish/fabulous.css">
+	<!-- 4 stylesheets just wasn't enough -->
+</head>
+<body>
+
+<div id="wrapper">
+	<h1>Behold! Weather.</h1>
+
+	<p>Stuff.</p>
+</div>
+
+</body>
+</html>
